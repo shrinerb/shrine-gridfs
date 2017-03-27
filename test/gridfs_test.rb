b17866b9f21e462d67835eece00fcf9579eb654c
Janko Marohnić <janko.marohnic@gmail.com>
2017-03-27 18:27:30 +1100

    Ensure that multiple files with same filename can be inserted

--- test/gridfs_test.rb
+++ test/gridfs_test.rb
@@ -21,4 +21,12 @@ describe Shrine::Storage::Gridfs do
   it "passes the linter" do
     Shrine::Storage::Linter.new(@gridfs).call
   end
+
+  it "allows inserting multiple files with the same filename" do
+    @gridfs.upload(fakeio("file1"), id1 = "foo", shrine_metadata: {"filename" => "file.ext"})
+    @gridfs.upload(fakeio("file2"), id2 = "bar", shrine_metadata: {"filename" => "file.ext"})
+
+    assert_equal "file1", @gridfs.open(id1).read
+    assert_equal "file2", @gridfs.open(id2).read
+  end
 end
